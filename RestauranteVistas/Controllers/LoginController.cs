b800942e339ee93da5bb6b5838bfbdd2f3c23726
Santiago Artavia Villanueva <santiagoartavia809@gmail.com>
2026-04-03 19:03:42 -0600

    pruebas

---
+++ RestauranteVistas/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace RestauranteVistasPro.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+// santi
---
+++ RestauranteVistas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestauranteVistas.MvcApplication" Language="C#" %>
