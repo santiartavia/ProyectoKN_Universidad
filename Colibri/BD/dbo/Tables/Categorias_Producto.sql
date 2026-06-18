442f5d2054db4e8de01ad6c76dda75625f626c28
xdicapua <sortegadcwk@gmail.com>
2026-06-18 15:49:09 -0600

    Carpeta Colibri Eliminada

--- Colibri/BD/dbo/Tables/Categorias_Producto.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Categorias_Producto] (
-    [id_categoria_prod] INT            IDENTITY (1, 1) NOT NULL,
-    [nombre_categoria]  NVARCHAR (100) NOT NULL,
-    [estado]            BIT            CONSTRAINT [DF_CatProd_estado] DEFAULT ((1)) NOT NULL,
-    CONSTRAINT [PK_Categorias_Producto] PRIMARY KEY CLUSTERED ([id_categoria_prod] ASC),
-    CONSTRAINT [UQ_CatProd_nombre] UNIQUE NONCLUSTERED ([nombre_categoria] ASC)
-);
-
